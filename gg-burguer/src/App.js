e5e033031a1f4e010f8e18a6543f350f6dd0cee1
gabriella-guimaraes <gabigara13@gmail.com>
2021-02-09 15:49:46 -0300

    Componentes iniciais da página de registro

--- gg-burguer/src/App.js
+++ gg-burguer/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained" color="primary">Olaaa</Button>
+      <Register></Register>
     </div>
   );
-}
+} 
 
 export default App;
