cc7a72bcb39c17aa859ca63ea5c4be27b560c8d5
gabriella-guimaraes <gabigara13@gmail.com>
2021-02-10 19:25:03 -0300

    fetch para registrar usuários funcionando!

---
+++ gg-burguer/src/pages/Kitchen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Kitchen() {
+    return(
+        <div class="kitchen-feed">
+            <h1>Feed da cozinha</h1>
+        </div>
+    );
+}
+
+export default Kitchen
--- gg-burguer/src/pages/Register.js
+++ gg-burguer/src/pages/Register.js
@@ -8,69 +8,90 @@ import { useHistory } from 'react-router-dom';
 function Register() {
 	const history = useHistory();
 
-	const routerConfirm = () => {
-		history.push('/registro');
+	const routerLogin = () => {
+		history.push('/login');
 	};
 
 	// const handleSubmit = (event) => {
 	// 	event.preventDefault;
-	// 	console.log("chegou até aqui, muito bem")
+	// 	console.log('chegou até aqui, muito bem');
 
-	// 	//fetch
-	// }
+	// 	fetch('https://lab-api-bq.herokuapp.com/users/', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			accept: 'application/json',
+	// 			'Content-Type': 'application/x-www-form-urlencoded'
+	// 		},
+	// 		body: `email=${email}.com&password=${password}&role=${role}&restaurant=GGBurger&name=${name}`
+	// 	})
+	// 		.then((response) => response.json())
+	// 		.then((json) => console.log(json));
+	// 		if(json.id !== null){
+	// 			//rota para o feed
+	// 			alert("Cadastro efetuado com sucesso! Bem vindo(a)")
+	// 			routerLogin();
+	// 		}
+	// };
 
-	const [ name, setName ] = useState("");
-	const [ email, setEmail ] = useState("");
-	const [ password, setPassword ] = useState("");
-	const [ role, setRole ] = useState("");
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ role, setRole ] = useState('');
 	return (
 		<div className="register">
 			<FormControl className="register">
-					<InputLabel required>Nome completo</InputLabel>
-					<Input
-					type="text"
-					value={name}
-					onChange={(event) => setName(event.target.value)}
-					>
-					</Input>
+				<InputLabel required>Nome completo</InputLabel>
+				<Input type="text" value={name} onChange={(event) => setName(event.target.value)} />
 			</FormControl>
 
 			<FormControl className="register">
-					<InputLabel required>Email</InputLabel>
-					<Input
-					type="text"
-					value={email}
-					onChange={(event) => setEmail(event.target.value)}
-					>
-					</Input>
+				<InputLabel required>Email</InputLabel>
+				<Input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
 			</FormControl>
 
 			<FormControl className="register">
-					<InputLabel required>Senha</InputLabel>
-					<Input
-					type="password"
-					value={password}
-					onChange={(event) => setPassword(event.target.value)}
-					>
-					</Input>
+				<InputLabel required>Senha</InputLabel>
+				<Input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
 			</FormControl>
 
 			<FormControl className="register">
-				<InputLabel required>Área de atendimento</InputLabel>
-				<Input
-				value={role}
-				type="text"
-				required
-				onChange={(event) => setRole(event.target.value)}
-				></Input>
+				<label required>Área de atendimento</label>
+				<select value={role} type="text" required onChange={(event) => setRole(event.target.value)}>
+					<option className="roleSelect">Salão</option>
+					<option className="roleSelect">Cozinha</option>
+				</select>
 			</FormControl>
-				<Button
-				 variant="contained"
-				 color="primary"
-				 id="finishRegister"
-				 type="submit"
-				 onClick={(event) =>  console.log("chegou até aqui, muito bem")}
-				 >Finalizar cadastro</Button>
+			<Button
+				variant="contained"
+				color="primary"
+				id="finishRegister"
+				type="submit"
+				onClick={(event) => {
+					event.preventDefault();
+					console.log('chegou até aqui, muito bem');
+
+					fetch('https://lab-api-bq.herokuapp.com/users/', {
+						method: 'POST',
+						headers: {
+							accept: 'application/json',
+							'Content-Type': 'application/x-www-form-urlencoded'
+						},
+						body: `email=${email}.com&password=${password}&role=${role}&restaurant=GGBurger&name=${name}`
+					})
+						.then((response) => response.json())
+						.then((json) => {
+							console.log(json);
+							if (json.id !== null) {
+								//rota para o feed
+								alert('Cadastro efetuado com sucesso! Bem vindo(a)');
+								routerLogin();
+							}
+						});
+					
+				}}
+			>
+				Finalizar cadastro
+			</Button>
 		</div>
 	);
 }
