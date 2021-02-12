d2883b45f4cfcf63522761310b9f110c50cacbd8
gabriella-guimaraes <gabigara13@gmail.com>
2021-02-12 16:32:29 -0300

    ajustes e resolvendo bugs

--- gg-burguer/src/components/Footer.js
+++ gg-burguer/src/components/Footer.js
@@ -1,17 +1,23 @@
 
 import React from 'react';
-//import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
 function Footer() {
 return(
-    <div className = "footer">
-    <Footer className="footer">
-    Desenvolvido por <a href="https://github.com/gabriella-guimaraes">Gabriella Guimarães</a> e 
-    <a href="https://github.com/GabrielleAlmeida"/>Gabrielle Almeida </a>
-    </Footer>
+    <div className="footer">
+        <Footer>
+            Desenvolvido por <Link to="https://github.com/gabriella-guimaraes">Gabriella Guimarães</Link>
+        </Footer>
+
     </div>
+    // <div className = "footer">
+    // <Footer className="footer">
+    //     Desenvolvido por <a href="https://github.com/gabriella-guimaraes">Gabriella Guimarães</a> e 
+    //     <a href="https://github.com/GabrielleAlmeida"/>Gabrielle Almeida </a>
+    // </Footer>
+    // </div>
 )
 };
 export default Footer;
--- gg-burguer/src/pages/Login.js
+++ gg-burguer/src/pages/Login.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import { FormControl, Input, InputLabel, Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header"
 
 function Login() {
   const history = useHistory();
+
+  const routerHall = () => {
+    history.push("/salao");
+  };
+  const routerKitchen = () => {
+    history.push("/cozinha");
+  }
   const routerRegister = () => {
     history.push("/registro");
-  };
+  }
+
   const [email, setEmail] = useState("");  
   const [password, setPassword] = useState("");
+
   return (
     <div className="login">
+      {/* <Header></Header> */}
       <p id="description"> Descrição do App </p>
 
       <FormControl className="login">
@@ -41,19 +52,6 @@ function Login() {
           variant="contained"
           color="primary"
           size="small"
-        >
-          Entar
-        </Button>
-      </FormControl>
-
-      <FormControl className="first-login">
-        <p>É o seu primeiro Acesso ? </p>
-        <Button
-          id="button-register"
-          type="submit"
-          variant="contained"
-          color="secondary"
-          size="small"
           onClick={(event) => {
             event.preventDefault();
               fetch("https://lab-api-bq.herokuapp.com/auth/", {
@@ -67,15 +65,34 @@ function Login() {
               .then((response) => response.json())
               .then((json) => {
                 console.log(json);
-                if (json.id !== null) 
-                  routerRegister(); 
+                if (json.role === "hall") {
+                  routerHall();
+                }else if (json.role === "kitchen") {
+                  routerKitchen();
+                }
+                  
                 });
           }}
+        >
+          Entar
+        </Button>
+      </FormControl>
+
+      <FormControl className="first-login">
+        <p>É o seu primeiro Acesso ? </p>
+        <Button
+          id="button-register"
+          type="submit"
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={routerRegister}
+          
           > Registrar-se
         </Button>
       </FormControl>
+      {/* <Footer></Footer> */}
     </div>
-    <Footer></Footer>
   );
 }
 
