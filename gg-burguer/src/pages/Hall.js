f67d4a7398c9c6754d403bbd57c7055a4fc43428
Gabrielle Almeida <gabi.antunes.almeida@gmail.com>
2021-02-18 15:08:56 -0300

    printando os produtos menu all-day na tela

--- gg-burguer/src/pages/Hall.js
+++ gg-burguer/src/pages/Hall.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import AllTimeMenu from '../components/All-TimeMenu';
+
+
+
 
 function Hall() {
-    const history = useHistory();
-    return(
-        <div class="hall-feed">
-            <h1>Feed do Salão</h1>
-        </div>
-    );
+	const history = useHistory();
+	return (
+		<div className="hall-feed">
+			<h1>Feed do salão</h1>
+			<Button variant="contained" color="primary" size="small">
+				Café da manhã
+			</Button>
+			<Button
+				id="button-all-day"
+				type="submit"
+				variant="contained"
+				color="primary"
+				size="small"
+				// onClick={(event) => {
+				// 	event.preventDefault();
+				// 	}
+				>Dia todo
+				</Button>
+				<AllTimeMenu/>
+			<div className="breakfast">
+				<p>O menu do café da manhã vai aparecer aqui</p>
+			</div>
+			<div className="allDay">
+				<p>O menu do dia todo vai aparecer aqui</p>
+			</div>
+			<div className="orders">
+				<p>Os pedidos serão contabilizados aqui</p>
+				<li>item</li>
+				<Button>Deletar item</Button>
+				<li>item</li>
+				<Button>Deletar item</Button>
+				<li>item</li>
+				<Button>Deletar item</Button>
+				<p>Total: $$</p>
+				<Button
+                	type="submit"
+                	variant="contained"
+                	color="primary" size="small">
+					Preparar
+				</Button>
+                <p>----------------------</p>
+                <h1>Lista de pedidos</h1>
+                <p>Os pedidos aparecerão aqui</p>
+			</div>
+			<div>
+				
+			</div>
+		</div>
+	);
 }
 
 export default Hall
--- gg-burguer/src/pages/Kitchen.js
+++ gg-burguer/src/pages/Kitchen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+//import { Button } from '@material-ui/core';
 
 function Kitchen() {
 	// const token = localStorage.getItem("token", token)
@@ -9,38 +9,7 @@ function Kitchen() {
 	return (
 		<div className="kitchen-feed">
 			<h1>Feed da cozinha</h1>
-			<Button variant="contained" color="primary" size="small">
-				Café da manhã
-			</Button>
-			<Button variant="contained" color="primary" size="small">
-				Dia todo
-			</Button>
-			<div className="breakfast">
-				<p>O menu do café da manhã vai aparecer aqui</p>
-			</div>
-			<div className="allDay">
-				<p>O menu do dia todo vai aparecer aqui</p>
-			</div>
-			<div className="orders">
-				<p>Os pedidos serão contabilizados aqui</p>
-				<li>item</li>
-				<Button>Deletar item</Button>
-				<li>item</li>
-				<Button>Deletar item</Button>
-				<li>item</li>
-				<Button>Deletar item</Button>
-				<p>Total: $$</p>
-				<Button
-                 type="submit"
-                 variant="contained"
-                 color="primary" size="small">
-					Preparar
-				</Button>
-                <p>----------------------</p>
-                <h1>Lista de pedidos</h1>
-                <p>Os pedidos aparecerão aqui</p>
-                
-			</div>
+			<p>Preparando....</p>
 		</div>
 	);
 }
